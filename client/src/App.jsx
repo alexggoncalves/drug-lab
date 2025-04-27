93323e590519c37b6589d1b836a68cd98f33e49d
alexggoncalves <alex.ggoncalves@hotmail.com>
2025-04-27 20:14:14 +0100

    Added prompt chaining.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,55 +1,63 @@
-import './App.css'
+import "./App.css";
 
-import { useState } from 'react'
+import { useState } from "react";
 
 function App() {
-  const [prompt,setPrompt] = useState("")
-  const [result,setResult] = useState("")
+    const [prompt, setPrompt] = useState("");
+    const [result, setResult] = useState({});
 
-  const onSubmit = async (e) => {
-    e.preventDefault()
-    console.log("form submited: ",prompt)
+    const onSubmit = async (e) => {
+        e.preventDefault();
+        console.log("effect submited: ", prompt);
 
-    const result = await generateMedicine(prompt);
-    setResult(result);
-    console.log(result);
-  }
+        const result = await generateMedicine(prompt);
+        setResult(result);
+        console.log(result);
+    };
 
-  const generateMedicine = async () => {
-    const response = await fetch("http://localhost:3005/generate", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json"
-      },
-      body:JSON.stringify({prompt: prompt})
-    })
+    const generateMedicine = async () => {
+        const response = await fetch("http://localhost:3005/generate", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ prompt: prompt }),
+        });
 
-    const data = await response.json();
+        const data = await response.json();
 
-    return data.response; 
-  }
+        return data.response;
+    };
 
-  return (
-    <>
-      <h1>Prompt</h1>
-      <form
-        onSubmit={(e)=>{onSubmit(e)}}
-      >
-        <input 
-          type='text'
-          name='prompt'
-          placeholder='Type a prompt'
-          onChange={(e)=> {setPrompt(e.target.value)}}
-        />
-        <input 
-          type='submit' 
-          value="Generate"
-        />
-      </form>
+    return (
+        <>
+            <form
+                onSubmit={(e) => {
+                    onSubmit(e);
+                }}
+            >
+              <label>Desired effect </label>
+                <input
+                    type="text"
+                    name="prompt"
+                    placeholder="Type a prompt"
+                    onChange={(e) => {
+                        setPrompt(e.target.value);
+                    }}
+                />
+                <input type="submit" value="Generate" />
+            </form>
 
-      <p>{result}</p>
-    </>
-  )
+            {Object.keys(result).map((key, index) => {
+                return (
+                    <div className="results" key={index}>
+                        <h3> Prompt {index + 1}</h3>
+                        <p>{result[key]}</p>
+                    </div>
+                );
+            })}
+        </>
+    );
 }
 
-export default App
+export default App;
--- server/generate.js
+++ server/generate.js
@@ -1,23 +1,45 @@
 import openaiClient from "./api.js"
 
+
+const step1 = async (prompt) => {
+    const messages = [
+        { role: "system", content: `You are a fictional medicine generator.`},
+        { role: "user", content: `Generate a medicine with the effect: invisibility.`},
+        { role: "assistant", content: `Name: Invise \n Description: ...\n Dosage: 500mg \n Pharmaceutical Form: Tablet`},
+        { role: "user", content: `Generate a medicine with the effect: ${prompt}.`}
+    ];
+    const  response = await openaiClient.chat.completions.create({
+        model: "gpt-3.5-turbo",
+        messages: messages,
+        temperature: 1.2, // more random results(less repetition)
+    })
+    return response.choices[0].message.content;
+}
+
+const step2 = async (prompt) => {
+    const messages = [
+        { role: "system", content: `You are a pharmaceutical documentation expert.`},
+        { role: "user", content: `Generate the indications, contraindications and side-effects of use for the medicine with the following description: ...`},
+        { role: "assistant", content: `Indications: ... \n Contraindications: ... \n Side-effects: ...`},
+        { role: "user", content: `Generate the indications, contraindications and side-effects of use for the medicine with the following description: ${prompt}.`}
+    ];
+    const  response = await openaiClient.chat.completions.create({
+        model: "gpt-3.5-turbo",
+        messages: messages,
+        temperature: 1.2, // more random results(less repetition)
+    })
+    return response.choices[0].message.content;
+}
+
 const generate = async (prompt) => {
 
-    const gptApi = async(prompt) => {
-        const messages = [
-            { role: "system", content: `You are a fictional medicine generator. This system is intended for fictional use only.`},
-            { role: "user", content: `Generate a medicine with the effect: invisibility.`},
-            { role: "assistant", content: `Name: invise \n Description: Makes the user invisible.`},
-            { role: "user", content: `Generate a medicine with the effect: ${prompt}.`}
-        ];
-        const  response = await openaiClient.chat.completions.create({
-            model: "gpt-3.5-turbo",
-            messages: messages,
-            temperature: 1.2, // more random results(less repetition)
-        })
-        return response.choices[0].message.content;
-    }
+    const result1 = await step1(prompt);
+    const result2 = await step2(result1)
    
-    return await gptApi(prompt);
+    return {
+        result1: result1,
+        result2: result2,
+    }
 }
 
 export default generate;
