ed3ed096d03752223210635353063adc32dd75af
alexggoncalves <alex.ggoncalves@hotmail.com>
2025-04-28 01:01:49 +0100

    Minr changes

--- server/generate.js
+++ server/generate.js
@@ -31,14 +31,31 @@ const step2 = async (prompt) => {
     return response.choices[0].message.content;
 }
 
+const step3 = async (result1,result2) => {
+    const messages = [
+        { role: "system", content: `You are a pharmaceutical documentation expert.`},
+        // { role: "user", content: `Generate the visual description of the package`},
+        // { role: "assistant", content: `Indications: ... \n Contraindications: ... \n Side-effects: ...`},
+        { role: "user", content: `Generate the visual description of the package with the following information: ${result1}; ${result2}.`}
+    ];
+    const  response = await openaiClient.chat.completions.create({
+        model: "gpt-3.5-turbo",
+        messages: messages,
+        temperature: 1.2, // more random results(less repetition)
+    })
+    return response.choices[0].message.content;
+}
+
 const generate = async (prompt) => {
 
     const result1 = await step1(prompt);
-    const result2 = await step2(result1)
+    const result2 = await step2(result1);
+    const result3 = await step3(result1,result2);
    
     return {
         result1: result1,
         result2: result2,
+        result3: result3,
     }
 }
 
